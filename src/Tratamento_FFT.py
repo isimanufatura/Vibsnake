96a88ba23e73515bf672c67d8848e71f988459a2
Matteo Zanetti <matteo.zanetti.mz@outlook.it>
2025-07-28 11:13:24 -0300

    .

--- src/Tratamento_FFT.py
+++ src/Tratamento_FFT.py
@@ -1,6 +1,7 @@
 import os
 import pandas as pd
 import func_tratamento as fft
+import numpy as np
 
 '''
 Código para o uso das funções de tratamento das FFTs
@@ -12,8 +13,8 @@ directory_path = "Exp_data"  # ex, "C:/Users/YourName/Documents"
 # Chamar a função de leitura e escolha
 arquivo = fft.escolha_arquivo(directory_path)
 
-# Exemplo de função tratada com corte
-fft.tratamento_FFT(arquivo=arquivo,corte=True,t_i=1,t_f=2)
+# # Exemplo de função tratada com corte
+# fft.tratamento_FFT(arquivo=arquivo,corte=True,t_i=1,t_f=2)
 
-# Exemplo de função tratada sem cort
+# Exemplo de função tratada sem corte
 fft.tratamento_FFT(arquivo=arquivo,corte=False)
